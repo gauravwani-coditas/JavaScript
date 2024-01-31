8fee9c1dc634354b4d305e34f669a00e48af5967
gauravwani-coditas <gaurav.wani@coditas.com>
2024-01-31 12:00:16 +0530

    init commit

---
+++ assignmentDay3.js
@@ -0,0 +1,14 @@
+
+
+//variables of different data types
+const fullName = "Gaurav Wani";
+const isEmployed = false;
+let score;
+const isHired = null;
+
+
+//typeof 
+console.log(typeof(fullName));
+console.log(typeof(isEmployed));
+console.log(typeof(score));
+console.log(typeof(isHired));
