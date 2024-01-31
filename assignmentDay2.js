e9f2b427e3e55676ea806ac525ad23c2f9688912
gauravwani-coditas <gaurav.wani@coditas.com>
2024-01-31 12:01:51 +0530

    file name changed

---
+++ assignmentDay2.js
@@ -0,0 +1,25 @@
+let apple;
+let mango;
+let banana;
+let orange;
+
+
+const employeeName = "Suyash" ;
+const employeePosition = "Associate Software Engineer Intern";
+const employeeSalary = 11000;
+const employeeDepartment = "Delivery-Product";
+
+const firstName = "Gaurav";
+const lastName = "Wani";
+const maritialStatus = "Single";
+const country = "India";
+const age = 21;
+
+
+const myFirstName = "Gaurav",myLastName = "Wani",myMaritialStatus = "Single",myCountry = "India",myCurrentAge = 21;
+
+const myAge = '21';
+const yourAge = '24';
+
+console.log(myAge);
+console.log(yourAge);
