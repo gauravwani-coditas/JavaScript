43e129e0041b3809b345cd0cd56d6ec07f95cdae
gauravwani-coditas <gaurav.wani@coditas.com>
2024-02-02 10:26:55 +0530

    codewar assignment

---
+++ assignmentCodewar2Feb.js
@@ -0,0 +1,16 @@
+function feast(beast, dish) {
+    //your function here
+      
+     distFirstIndex = dish[0];
+     dishLastIndex = dish[dish.length-1];
+      
+      beastFirstIndex = beast[0];
+      beastLastIndex = beast[beast.length-1];
+      
+      if(beastFirstIndex === distFirstIndex && beastLastIndex === dishLastIndex){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
---
+++ day3.js
@@ -0,0 +1,32 @@
+const name = "Gaurav Wani";
+const greet = "Hello!";
+
+console.log(`${greet}, I am ${name}`)
+console.log(`${greet}, ${name}`);
+console.log(name[2]);
+console.log(name.charCodeAt(2));
+
+let arr= name.split('-');
+console.log(arr);
+console.log
+console.log(greet .toLocaleLowerCase());
+
+let str = "JavaScript";
+// console.log(str.substring(4,6)); //end index in exclusive.
+// console.log(str.slice(4,6));
+// console.log(str.substr(4,6));
+// str.trim();
+
+
+
+str.slice(4,6);
+console.log(str);
+
+str.substr(4,2);
+console.log(str);
+//let b = str.substring(4,6);
+//console.log(a+' ');
+
+console.log(str.indexOf('a'));
+console.log(str.lastIndexOf('a'));
+
