d9da13fab90e48ac30eb9bd207582de08b5ed1d0
gauravwani-coditas <gaurav.wani@coditas.com>
2024-02-02 10:37:22 +0530

    first commit

--- assignmentCodewar2Feb.js
+++ assignmentCodewar2Feb.js
@@ -1,16 +1,14 @@
 function feast(beast, dish) {
-    //your function here
-      
-     distFirstIndex = dish[0];
-     dishLastIndex = dish[dish.length-1];
-      
-      beastFirstIndex = beast[0];
-      beastLastIndex = beast[beast.length-1];
-      
-      if(beastFirstIndex === distFirstIndex && beastLastIndex === dishLastIndex){
-        return true;
-      }
-      else{
-        return false;
-      }
+  //your function here
+    
+   distFirstIndex = dish[0];
+   dishLastIndex = dish[dish.length-1];
+    
+    beastFirstIndex = beast[0];
+    beastLastIndex = beast[beast.length-1];
+    
+    if(beastFirstIndex === distFirstIndex && beastLastIndex === dishLastIndex){
+      return true;
     }
+    return false;
+}
