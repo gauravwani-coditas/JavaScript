6b55d92a52a98a3e715300c6fde8b5762a1b82e6
gauravwani-coditas <gaurav.wani@coditas.com>
2024-02-19 13:52:04 +0530

    f

--- pattern.js
+++ pattern.js
@@ -23,4 +23,27 @@ for(let rowIndex = 0;rowIndex < mid;rowIndex++){
    console.log(row);
 
 }
+for(let rowIndex = 0;rowIndex < mid-1;rowIndex++){
+
+    let row = " ";
+    
+
+    for(let colIndex1 = 0;colIndex1<10;colIndex1++){
+
+        if(colIndex1 === rowIndex){
+            row = row + "*";
+        }
+        // else if(colIndex1 === 10-rowIndex-4 ){
+        //     row = row+"*";
+        // }
+        else{
+            row = row + " ";
+        }
+
+
+    }
+
+   console.log(row);
+
+}
     
