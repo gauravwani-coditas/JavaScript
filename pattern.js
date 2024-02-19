edd18b6aa60c86cf96127f059290ead84bd65465
gauravwani-coditas <gaurav.wani@coditas.com>
2024-02-19 11:33:41 +0530

    First commit

--- pattern.js
+++ pattern.js
@@ -6,37 +6,13 @@ for(let rowIndex = 0;rowIndex < rowVal;rowIndex++){
     let row = '';
     let colIndex1 = 0;
 
-    while(colIndex1 < rowVal){
+    while(colIndex1 < rowVal-rowIndex){
 
-        if(colIndex1 === rowVal-rowIndex){
-            row = row + "";
-        }
-        else{
-            row  = row + " ";
-        }
-    colIndex1++;
+        row = row+" ";
+        colIndex1++;
    }
-
-//    while(colIndex2 < rowVal){
-
-//     if(colIndex2 === rowVal-rowIndex){
-//         row = row + "";
-//     }
-//     else{
-//         row  = row + " ";
-//     }
-// colIndex1++;
-}
+   row = row+"*";
    console.log(row);
-//    while(colIndex1 < 10){
 
-//     if(colIndex === rowVal-colIndex1){
-//         row+= "*";
-//     }
-//     else{
-//         row+= " ";
-//     }
-//    }
-    
-    
 }
+    
